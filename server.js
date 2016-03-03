0e6660e9bb65c1576422af316d5d71653d095579
Mike Ulvila <mikeulvila@me.com>
2016-03-03 14:08:13 -0600

    connected to database

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const app = express();
+const pg = require('pg').native;
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 3000;
-
+const POSTGRES_URL = process.env.POSTGRES_URL || 'postgres://localhost:5432/nodechat';
+let db;
 
 app.set('view engine', 'jade');
 app.use(express.static('public'));
@@ -15,8 +17,24 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
+app.get('/chats', (req, res) => {
+  db.query('SELECT * FROM chats', (err, result) => {
+    console.log('>>>>>', err, result.rows);
+
+    if (err) throw err;
+
+    res.send(result.rows);
+  });
+});
+
+pg.connect(POSTGRES_URL, (err, client) => {
+  if (err) throw err
+
+  db = client
+
+  server.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+  });
 });
 
 io.on('connection', (socket) => {
